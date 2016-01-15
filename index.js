94ba4d9d976b184b9841becb025efe14f8c3d685
Andrey Belym <belym.a.2105@gmail.com>
2016-01-15 17:57:36 +0300

    Implement autocanceling `race` function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Promise = require('pinkie-promise');
 
-module.exports = function (emitter, event) {
+function promisifyEvent (emitter, event) {
     var listener = null;
 
     var promise = new Promise(function (resolve, reject) {
@@ -27,4 +27,78 @@ module.exports = function (emitter, event) {
     };
 
     return promise;
-};
+}
+
+function _isThenable (obj) {
+    return typeof obj.then === 'function';
+}
+
+function _isEmitter (obj) {
+    return typeof obj.once === 'function';
+}
+
+function _parseRaceArgs (args) {
+    var n = args.length;
+    var i = 0;
+    var promises = [];
+
+    while (i < n) {
+        if (_isThenable(args[i])) {
+            promises.push(args[i]);
+            i += 1;
+            continue;
+        }
+
+        if (_isEmitter(args[i]) && i + 1 < n && typeof args[i+1] === 'string') {
+            promises.push(promisifyEvent(args[i], args[i+1]));
+            i += 2;
+            continue;
+        }
+
+        throw new Error('Wrong arguments');
+    }
+
+    return promises;
+}
+
+function _getPromisesCancellations (promises) {
+    return promises.map(function (promise){
+        if (typeof promise.cancel !== 'function')
+            return void 0;
+
+        return promise.cancel();
+    });
+}
+
+function _wrapRacePromises (promises) {
+    return promises.map(function (promise, index) {
+        return promise
+            .then(function (result){
+                return { index: index, result: result };
+            })
+            .catch(function (error){
+                return { index: index, error: error }
+            });
+    });
+}
+
+function raceEvents (args) {
+    var promises = _parseRaceArgs(args);
+
+    return Promise
+        .race(_wrapRacePromises(promises))
+        .then(function (result) {
+            promises.splice(result.index, 1);
+
+            return Promise.all(_getPromisesCancellations(promises))
+                .then(function () {
+                    if (result.error)
+                        throw result.error;
+
+                    return result.result;
+                });
+        });
+}
+
+module.exports = promisifyEvent;
+module.exports.race = raceEvents;
--- test/test.js
+++ test/test.js
@@ -63,3 +63,76 @@ it('Should cancel event promise and subscription', function () {
         promise
     ]);
 });
+
+describe('Race', function () {
+    it('Should race multiple events', function () {
+        var emitter = new EventEmitter();
+
+        var promise = promisifyEvent.race([
+            emitter, 'test-event1',
+            emitter, 'test-event2',
+            emitter, 'test-event3'
+        ]);
+
+        emitter.emit('test-event2', 'test2', 'data2');
+
+        return promise
+            .then(function (res) {
+                assert.deepEqual(res, ['test2', 'data2']);
+                assert.strictEqual(emitter.listeners('test-event1').length, 0);
+                assert.strictEqual(emitter.listeners('test-event2').length, 0);
+                assert.strictEqual(emitter.listeners('test-event3').length, 0);
+            })
+    });
+
+    it('Should race multiple events along with error', function () {
+        var emitter = new EventEmitter();
+
+        var promise = promisifyEvent.race([
+            emitter, 'test-event1',
+            emitter, 'test-event2',
+            emitter, 'error'
+        ]);
+
+        emitter.emit('error', 'error-data');
+
+        return promise
+            .then(function () {
+                throw new Error('Promise rejection expected!');
+            })
+            .catch(function (err) {
+                assert.strictEqual(err, 'error-data');
+                assert.strictEqual(emitter.listeners('test-event1').length, 0);
+                assert.strictEqual(emitter.listeners('test-event2').length, 0);
+                assert.strictEqual(emitter.listeners('test-event3').length, 0);
+            })
+    });
+
+    it('Should race multiple events along with another promises', function () {
+            var emitter = new EventEmitter();
+
+            var promise = promisifyEvent.race([
+                emitter, 'test-event1',
+                new Promise(function (resolve) {
+                    setTimeout(function () {
+                        resolve('timeout1')
+                    }, 100);
+                }),
+                emitter, 'test-event2',
+                new Promise(function (resolve) {
+                    setTimeout(function () {
+                        resolve('timeout2')
+                    }, 150);
+                }),
+                emitter, 'test-event3'
+            ]);
+
+            return promise
+                .then(function (res) {
+                    assert.strictEqual(res, 'timeout1');
+                    assert.strictEqual(emitter.listeners('test-event1').length, 0);
+                    assert.strictEqual(emitter.listeners('test-event2').length, 0);
+                    assert.strictEqual(emitter.listeners('test-event3').length, 0);
+                })
+        });
+});
